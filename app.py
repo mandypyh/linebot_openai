2cbb1816f445e3737593e3fe5325165f4e34bbff
mandypyh <mandy710522@gmail.com>
2023-05-24 21:34:08 +0800

    removereplace

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def GPT_response(text):
     response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
     print(response)
     # 重組回應
-    answer = response['choices'][0]['text'].replace('\n','')
+    answer = response['choices'][0]['text']
     return answer
 
 # 處理訊息
